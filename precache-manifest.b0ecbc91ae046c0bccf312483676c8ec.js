3991a75a956f31e557def6c963140cc1aafee198
Dorothy N <you@example.com>
2020-10-26 20:30:17 +0100

    Updates

---
+++ precache-manifest.b0ecbc91ae046c0bccf312483676c8ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eea2a4b2fce22540626d2af4d8963590",
+    "url": "/Robo-pages/index.html"
+  },
+  {
+    "revision": "6bc907bcf4ba24690fbd",
+    "url": "/Robo-pages/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2865d16e1a6d258f9be1",
+    "url": "/Robo-pages/static/css/main.a8d5204f.chunk.css"
+  },
+  {
+    "revision": "6bc907bcf4ba24690fbd",
+    "url": "/Robo-pages/static/js/2.5f80a749.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/Robo-pages/static/js/2.5f80a749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2865d16e1a6d258f9be1",
+    "url": "/Robo-pages/static/js/main.ce2d1205.chunk.js"
+  },
+  {
+    "revision": "e9592408d7b65b7aec72",
+    "url": "/Robo-pages/static/js/runtime-main.d9553b68.js"
+  },
+  {
+    "revision": "696c22ee9d3d7bc00cd8e8916358e67a",
+    "url": "/Robo-pages/static/media/major_shift.696c22ee.woff"
+  }
+]);
